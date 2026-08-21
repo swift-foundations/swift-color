77d86b9db0b47dbcf5f84851a62c4f2be995ed07
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-21 15:50:42 +0200

    Remove source comments and DocC

--- Tests/Color Tests/Color Tests.swift
+++ Tests/Color Tests/Color Tests.swift
@@ -1,14 +1,3 @@
-// ===----------------------------------------------------------------------===//
-//
-// This source file is part of the swift-color open source project
-//
-// Copyright (c) 2025 Coen ten Thije Boonkkamp and the swift-color project authors
-// Licensed under Apache License v2.0
-//
-// See LICENSE for license information
-//
-// ===----------------------------------------------------------------------===//
-
 import Color
 import Testing
 
